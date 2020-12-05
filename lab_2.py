e029e525f7320027b82fc1efb619491b2409f63b
yura454 <72279079+yura454@users.noreply.github.com>
2020-12-05 13:00:46 +0200

    я молодец

---
+++ lab_2.py
@@ -0,0 +1,7 @@
+n = int(input("введите число n: "))
+q0 = int(input("введите число q0: "))
+q1 = int(input(" введите число q1: "))
+for i in range(0,n+1,1):
+    q=q0*q1
+    print(q)
+    q0=q
---
+++ lab_3.py
@@ -0,0 +1,5 @@
+year = int(input("введите год year: "))
+if (year % 4 == 0):
+    print('високосный')
+else:
+    print('0')
